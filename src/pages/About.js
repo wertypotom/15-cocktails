cee0120afc0b2766af0499188327e83acb359dbe
wertypotom <werty.potom@gmail.com>
2021-08-03 16:44:34 +0600

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam
+        consequatur ratione officia perspiciatis veniam quas eum recusandae
+        assumenda soluta placeat laboriosam totam, architecto velit, repudiandae
+        porro suscipit illum! Enim fugiat laudantium quae aspernatur ut commodi,
+        molestiae architecto provident necessitatibus ratione!
+      </p>
+    </section>
+  );
+};
+
+export default About;
